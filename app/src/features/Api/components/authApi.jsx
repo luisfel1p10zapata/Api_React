ca3b345323ecca843efc09bdf705565cfaff5b60
Luis Zapata <luisfel1p.10zapata@gmail.com>
2026-04-12 17:51:02 -0500

    fix api url directa

--- app/src/features/Api/components/authApi.jsx
+++ app/src/features/Api/components/authApi.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_URL = "https://api-react-79xs.onrender.com/api";
 
+console.log("API_URL:", API_URL); // Verificar que la URL es correcta
+
 export const registerUser = (data) =>
   axios.post(`${API_URL}/auth/register`, data);
 
