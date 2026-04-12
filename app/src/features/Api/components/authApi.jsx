801c1c91eaa8445908b7cbc61d0d96ffd42800c0
Luis Zapata <luisfel1p.10zapata@gmail.com>
2026-04-12 17:36:34 -0500

    force rebuild env

--- app/src/features/Api/components/authApi.jsx
+++ app/src/features/Api/components/authApi.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 console.log("API_URL:", import.meta.env.VITE_API_URL);
+// solo agrega esto temporalmente
+console.log("rebuild");
 
 export const registerUser = (data) =>
   axios.post(`${API_URL}/auth/register`, data);
