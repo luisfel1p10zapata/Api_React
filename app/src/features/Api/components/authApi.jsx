157eb309f2e800379b1cebd87e42b93ddc971ff3
Luis Zapata <luisfel1p.10zapata@gmail.com>
2026-04-12 16:47:37 -0500

    conexion a backend en produccion

--- app/src/features/Api/components/authApi.jsx
+++ app/src/features/Api/components/authApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api/auth";
+const API_URL = import.meta.env.VITE_API_URL + "/auth";
 
 export const registerUser = (data) =>
   axios.post(`${API_URL}/register`, data);
